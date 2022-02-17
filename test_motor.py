2e6d8aa7eca8aa0d3cd5ec17ec832d2965d5f686
Kevin Ahlstrom <ikeavin@gmail.com>
2022-02-16 17:07:34 -0700

    test both motors at once

--- test_motor.py
+++ test_motor.py
@@ -2,27 +2,39 @@ import pyvesc
 import time
 import serial
 
+#Left side motors
+#Move forward with negative current
+leftPort = serial.Serial(port='/dev/ttyACM0', 
+		     baudrate=11520, 
+		     parity=serial.PARITY_NONE,
+		     timeout = .1,
+             bytesize=serial.EIGHTBITS)
+    
+#Right side motors
+#Move forward with positive current
+rightPort = serial.Serial(port='/dev/ttyACM1', 
+		     baudrate=11520, 
+		     parity=serial.PARITY_NONE,
+		     timeout = .1,
+             bytesize=serial.EIGHTBITS)
+
+#Convert current into bytes that can be transmitted to the VESC             
 def pack(value) -> bytes:
     message = pyvesc.SetCurrent(value)
     packet = pyvesc.encode(message)
     return packet
 
-
-port = serial.Serial(port='/dev/ttyACM0', 
-		     baudrate=11520, 
-		     parity=serial.PARITY_NONE,
-		     timeout = .1,
-             bytesize=serial.EIGHTBITS)
-'''	     
-port2 = serial.Serial(port='/dev/ttyACM', 
-		     baudrate=11520, 
-		     parity=serial.PARITY_NONE,
-		     timeout = .1,
-             bytesize=serial.EIGHTBITS)
-'''            
-while 1 == 1:
-    current = -30000
-    packet = pack(current)
-    port.write(packet)
-    port.flush()
-
+#Drive forward for specified amount of seconds
+def driveForward(seconds):
+    endTime = time.time
+    while time.time < endTime + seconds:
+        leftCurrent = -30000
+        rightCurrent = -leftCurrent
+        leftPacket = pack(leftCurrent)
+        rightPacket = pack(rightCurrent)
+        leftPort.write(leftPacket)
+        rightPort.write(rightPacket)
+        leftPort.flush()
+        rightPort.flush()
+            
+driveForward(10)
